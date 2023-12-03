e0b7e13aaa8e4394b5e866fab51050f34277564b
jhordanjulcamoroms4m <jhordanjulcamoro@ms4m.com>
2023-12-03 15:08:07 -0500

    add googlemap into mapclientactivity

--- app/src/main/java/com/julhua/motti/activities/client/MapClientActivity.java
+++ app/src/main/java/com/julhua/motti/activities/client/MapClientActivity.java
@@ -1,16 +1,30 @@
 package com.julhua.motti.activities.client;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.julhua.motti.R;
 
-public class MapClientActivity extends AppCompatActivity {
-
+public class MapClientActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap nMap;
+    private SupportMapFragment mMapFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_client);
+
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        nMap = googleMap;
+        nMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 }
